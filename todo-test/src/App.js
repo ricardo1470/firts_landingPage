e1a425c35b8b95572abff095a7b6ad43364d3d9b
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-12-16 17:00:47 -0500

    create files, test tailwind v3.0

---
+++ todo-test/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <button className="rounded-full border-double bg-indigo-500 h-40 w-40 mr-8 cursor-grabbing border-green-700">click me!</button>
+
+
+  );
+}
+
+export default App;
